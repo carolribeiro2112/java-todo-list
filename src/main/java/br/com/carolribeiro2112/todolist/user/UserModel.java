34e95e5aac5b444293a5ccc9d9dc3a730bab2810
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-10-10 08:45:04 -0300

    Feat: Add lombok to the project

--- src/main/java/br/com/carolribeiro2112/todolist/user/UserModel.java
+++ src/main/java/br/com/carolribeiro2112/todolist/user/UserModel.java
@@ -1,8 +1,13 @@
 package br.com.carolribeiro2112.todolist.user;
 
+import lombok.Data;
+
+// @Data --> adiciona todos os getters e setters automaticamente
+@Data
 public class UserModel {
 
   private String userName;
   private String name;
   private String password;
+
 }
